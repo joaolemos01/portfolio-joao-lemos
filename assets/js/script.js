f1d176d9741efdf208d9e87710d3f3d7de56e659
João Lemos <joaogabrielbatistalemos@gmail.com>
2026-02-20 22:44:03 -0300

    ALTERÇÃOES JS (MENU | SCROLL)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,54 @@
-console.log("JS running ✅");
-
+// Seleciona o Menu
 const navLinks = document.querySelectorAll(".nav__link");
-console.log("Links found:", navLinks.length);
-
-navLinks.forEach(function(link) {
-  link.addEventListener("click", function() {
-    console.log("Clicked:", link.getAttribute("href"));
-
-    navLinks.forEach(function(item) {
-      item.classList.remove("active");
-    });
-
-    link.classList.add("active");
+// MENU
+navLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    
+    navLinks.forEach(item => item.classList.remove("active"));
+    
+    this.classList.add("active");
   });
+});
+
+// Seleciona todas as sections
+const sections = document.querySelectorAll("section");
+// Escuta evento de scroll
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach(section => {
+    
+    const sectionTop = section.offsetTop;        // posição da section no topo da página
+    const sectionHeight = section.clientHeight;  // altura da section
+    
+    const headerHeight = header.offsetHeight;    // altura do header fixo
+
+    // Ajusta cálculo considerando o header fixo
+    if (window.scrollY >= sectionTop - headerHeight - sectionHeight / 4) {
+      currentSection = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
+
+// Seleciona o header
+const header = document.querySelector(".header");
+// Escuta evento de scroll
+window.addEventListener("scroll", function () {
+  
+  // Se a página estiver rolada mais que 50px
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");  // adiciona classe
+  } else {
+    header.classList.remove("scrolled"); // remove classe
+  }
+
 });
